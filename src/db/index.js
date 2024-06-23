18d4a0558e8dd7d62f8b66006501b0070962d85e
Aditya Saini <adityasaini@Adityas-MacBook-Air-3.local>
2024-06-23 18:17:19 +0530

    user and video model done

---
+++ src/db/index.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants";
+
+;(async () => {
+    try{
+        
+    }
+    catch (error) {
+        console.log("ERROR While connecting to MONGODB",  error);
+        process.exit(1);
+    }
+})()
